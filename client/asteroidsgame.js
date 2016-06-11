a64d2ad855243aadb91f2a63fc68a1e7c83bccfe
Kubo Motyl <motyl.kubo@gmail.com>
2016-06-11 20:34:58 +0200

    add projectiles, rework redrawing

--- client/asteroidsgame.js
+++ client/asteroidsgame.js
@@ -1,25 +1,9 @@
-TIME_PER_FRAME = 1000 / 30 // 30 fps
-ASTEROID_PEN = {
-  fill: '#000',
-  stroke: {
-    color: '#fff',
-    width: 1
-  }
-}
-PLAYER_PEN = {
-  fill: '#000',
-  stroke: {
-    color: '#fff',
-    width: 2
-  }
-}
-
 // Game class ----------------------------------------------------------
 function Game(){
   this.rootElement = document.createElement('div')
   this.rootElement.style.position = 'absolute'
   this.rootElement.style.width = '640px'
-  this.rootElement.style.height = '640px'
+  this.rootElement.style.height = '480px'
 }
 
 Game.prototype.show = function(){
@@ -38,12 +22,15 @@ function AsteroidsGame(cfg){
   this.rootElement.appendChild(this.canvas)
 
   this.ctx = this.canvas.getContext('2d')
-  this.keyPressed = { 'up' : false, 'left': false, 'right': false }
+  this.keyPressed = { 'up' : false, 'left': false, 'right': false, 'fire': false }
   var keyMappings = {
     38: 'up', 87: 'up',
     37: 'left', 65: 'left',
-    39: 'right', 68: 'right'
+    39: 'right', 68: 'right',
+    32: 'fire', 13: 'fire'
   }
+  this.time = 0
+
   var self = this
   this.canvas.addEventListener('keydown', function(event){
     if (keyMappings.hasOwnProperty(event.keyCode))
@@ -55,13 +42,32 @@ function AsteroidsGame(cfg){
       self.keyPressed[keyMappings[event.keyCode]] = false
   })
 
-  this.resize()
-  this.state = {
-    alive_time: 0,
-    asteroids_destroyed: 0,
-    asteroids_info: [],
-    player_info: []
+  this.TIME_PER_FRAME = 1000 / 30 // 30 fps
+  this.PEN = {
+    ASTEROID: {
+      fill: '#000',
+      stroke: {
+        color: '#fff',
+        width: 1
+      }
+    },
+    PLAYER: {
+      fill: '#000',
+      stroke: {
+        color: '#fff',
+        width: 2
+      }
+    },
+    PROJECTILE: {
+      fill: '#fff',
+      stroke: {
+        color: '#fff',
+        width: 1
+      }
+    }
   }
+
+  this.adjustSize()
   this.start()
 }
 
@@ -69,22 +75,31 @@ AsteroidsGame.prototype = Object.create(Game.prototype)
 AsteroidsGame.prototype.constructor = Game
 
 AsteroidsGame.prototype.resize = function(){
+  // divide everything (pos+surf) by former scale
+  // asteroids, player, projectiles, explosions,
+  this.adjustSize()
+  // multiply everything by new scale
+}
+
+AsteroidsGame.prototype.adjustSize = function(){
   this.canvas.setAttribute('width', this.rootElement.style.width)
   this.canvas.setAttribute('height', this.rootElement.style.height)
 
-  this.screenWidth = parseInt(this.canvas.getAttribute('width'))
-  this.screenHeight = parseInt(this.canvas.getAttribute('height'))
+  this.screen = {
+    width: parseInt(this.canvas.getAttribute('width')),
+    height: parseInt(this.canvas.getAttribute('height'))
+  }
+  this.screen.scale = Math.min(this.screen.width, this.screen.height)
 }
 
 AsteroidsGame.prototype.setState = function(gameState){
-  this.alive = gameState.alive_time
   this.destroyed = gameState.asteroids_destroyed
   this.asteroids = gameState.asteroids_info
   this.player = gameState.player_info
 }
+
 AsteroidsGame.prototype.getState = function(){
   return {
-    alive_time: this.alive,
     asteroids_destroyed: this.destroyed,
     asteroids_info: this.asteroids,
     player_info: this.player
@@ -92,88 +107,134 @@ AsteroidsGame.prototype.getState = function(){
 }
 
 AsteroidsGame.prototype.start = function(){
-  this.alive = 0
-  this.destroyed = 0
+  this.destroyed = []
   this.asteroids = []
-  this.asteroids.push(new Asteroid(3))
-  this.player = new Player()
+  this.player = new Player(this.screen)
+  this.projectiles = []
 
   var self = this
   this.loopTimer = setInterval(
     function(){ self.gameLoop() },
-    TIME_PER_FRAME
+    this.TIME_PER_FRAME
   )
+  // setTimeout(function(){ self.stop() }, 5000) // @todo remove this
 }
+
 AsteroidsGame.prototype.stop = function(){
   // stops game and shows game menu overlay
   clearInterval(this.loopTimer)
 }
 
 AsteroidsGame.prototype.gameLoop = function(){
-  if (this.keyPressed['up'])
-    console.log('up')
-  if (this.keyPressed['left'])
-    this.player.rotate(-5 / 180 * Math.PI)
-  if (this.keyPressed['right'])
-    this.player.rotate(5 / 180 * Math.PI)
+  this.time += this.TIME_PER_FRAME
 
-  // increase time counter
-  this.alive += TIME_PER_FRAME / 10
+  this.handleInput()
+  this.simulatePhysics()
 
-  // move asteroids and player (destroy asteroids on 'out of screen')
-  outOfScreen = []
-  for (var i = 0; i < this.asteroids.length; i++) {
-    this.asteroids[i].move(this.screenWidth, this.screenHeight)
-    if (!this.asteroids[i].onScreen(this.screenWidth, this.screenHeight))
-      outOfScreen.push(i)
+  // compute and resolve collisions
+  // for (var i = 0; i < this.asteroids.length; i++)
+  //   if (collides(this.player, this.asteroids[i]))
+  //     console.log('collision with asteroid', this.asteroids[i])
+
+  this.redraw()
+}
+
+AsteroidsGame.prototype.handleInput = function(){
+  if (this.keyPressed['up']){
+    this.player.speed = this.player.speed.add(
+      new Vector(
+        Math.cos(this.player.direction),
+        Math.sin(this.player.direction)
+      ).multiply(this.player.force)
+    )
+
+    if (this.player.speed.magnitude() > this.player.maxForce)
+      this.player.speed = this.player.speed.normalized().multiply(this.player.maxForce)
   }
-  for (var i = 0; i < outOfScreen.length; i++)
-    this.asteroids.splice(outOfScreen[i]-i, 1)
+  if (this.player.position.x < 0 || this.player.position.x > 1)
+    this.player.position.x = 1 - this.player.position.x
+  if (this.player.position.y < 0 || this.player.position.y > 1)
+    this.player.position.y = 1 - this.player.position.y
+
+  if (this.keyPressed['left']){
+    this.player.direction -= 10 / 180 * Math.PI
+    this.player.rotate(-10 / 180 * Math.PI, this.screen)
+  }
+  if (this.keyPressed['right']){
+    this.player.direction += 10 / 180 * Math.PI
+    this.player.rotate(10 / 180 * Math.PI, this.screen)
+  }
+
+  if (this.keyPressed['fire'] && this.projectiles.length < 10){
+    this.projectiles.push(new Projectile(
+      this.player.position.add(this.player.surface[0]),
+      -this.player.direction,
+      this.screen
+    ))
+  }
+}
+
+AsteroidsGame.prototype.simulatePhysics = function(){
+  offScreen = []
+  for (var i = 0; i < this.asteroids.length; i++){
+    move(this.asteroids[i], this.screen)
+    if (!this.asteroids[i].onScreen())
+      offScreen.push(i)
+  }
+  for (var i = 0; i < offScreen.length; i++)
+    this.asteroids.splice(offScreen[i]-i, 1)
 
   this.score = computeScore(null, this.getState())
   // add new asteroids if needed
-  for (var i = 0; i < (baseLog(5, this.score+1) - this.asteroids.length + 1); i++) {
-    this.asteroids.push(new Asteroid(baseLog(15, this.score+1)))
+  for (var i = 0; i < (baseLog(5, this.score+1) - this.asteroids.length + 1); i++)
+    this.asteroids.push(new Asteroid(baseLog(15, this.score+1) + 3, this.screen))
+
+  move(this.player, this.screen)
+
+  offScreen = []
+  for (var i = 0; i < this.projectiles.length; i++){
+    move(this.projectiles[i], this.screen)
+    if (!this.projectiles[i].onScreen())
+      offScreen.push(i)
   }
-
-  // compute and resolve collisions
-
-  this.redraw()
+  for (var i = 0; i < offScreen.length; i++)
+    this.projectiles.splice(offScreen[i]-i, 1)
 }
 
 AsteroidsGame.prototype.redraw = function(){
   // black background
   this.ctx.fillStyle = '#000'
-  this.ctx.fillRect(0, 0, this.screenWidth, this.screenHeight)
+  this.ctx.fillRect(0, 0, this.screen.width, this.screen.height)
 
   // asteroids
-  for (var i = 0; i < this.asteroids.length; i++) {
-    draw(
-      this.asteroids[i], this.ctx,
-      this.screenWidth, this.screenHeight, ASTEROID_PEN
-    )
-  }
+  for (var i = 0; i < this.asteroids.length; i++)
+    draw(this.asteroids[i], this.ctx, this.PEN.ASTEROID)
+
+  // player
+  draw(this.player, this.ctx, this.PEN.PLAYER)
+
+  // projectiles
+  for (var i = 0; i < this.projectiles.length; i++)
+    draw(this.projectiles[i], this.ctx, this.PEN.PROJECTILE)
 
   // explosions
 
-  // player
-  draw(this.player, this.ctx, this.screenWidth, this.screenHeight, PLAYER_PEN)
 }
 
 function computeScore(cfg, gameState){
   try {
-    return gameState.alive_time + gameState.asteroids_destroyed * 50
-  } catch (error) {
+    return gameState.asteroids_destroyed * 50 // @todo remake this
+  } catch (error){
     console.error(error)
     return -1
   }
 }
 
 // Asteroid class ------------------------------------------------------
-function Asteroid(speed){
+function Asteroid(speed, screen){
   this.speed = speed
   this.direction = (
-    25 + Math.random() * 40 + parseInt(Math.random() * 4) * 90
+    20 + Math.random() * 50 + parseInt(Math.random() * 4) * 90
   ) / 180 * Math.PI
   this.reach = 0.025 + Math.random() * 0.025
 
@@ -197,7 +258,7 @@ function Asteroid(speed){
   var start = Math.random() * 2 * Math.PI
   this.surface = [new Vector(Math.cos(start), Math.sin(start))]
   for (var i = 1; i < 360; i++){
-    var offset = this.surface[this.surface.length-1].length()
+    var offset = this.surface[this.surface.length-1].magnitude()
     offset += (Math.random() - 0.5) * 0.1
 
     offset = Math.min(1, offset)
@@ -205,35 +266,24 @@ function Asteroid(speed){
     var d = start + (i / 180 * Math.PI)
     this.surface.push(new Vector(Math.cos(d), Math.sin(d)).multiply(offset))
   }
-  var self = this
+  var reach = this.reach
   this.surface = this.surface.map(function(v){
-    return v.multiply(self.reach)
+    return v.multiply(reach)
   })
+
+  this.render = getRender(this, screen)
 }
 
-Asteroid.prototype.move = function(screenWidth, screenHeight){
-  this.position = this.position.add(
-    new Vector(
-      Math.cos(this.direction) / screenWidth,
-      -Math.sin(this.direction) / screenHeight
-    ).multiply(this.speed)
-  )
-}
-
-Asteroid.prototype.onScreen = function(width, height){
+Asteroid.prototype.onScreen = function(){
   var checks = [
-    function(p, r, w, h){ return p.x + r.x > 0 },
-    function(p, r, w, h){ return p.x - r.x < w },
-    function(p, r, w, h){ return p.y + r.y > 0 },
-    function(p, r, w, h){ return p.y - r.y < h }
+    function(p, r){ return p.x + r > 0 },
+    function(p, r){ return p.x - r < 1 },
+    function(p, r){ return p.y + r > 0 },
+    function(p, r){ return p.y - r < 1 }
   ]
-  var position = this.position.multiply(1)
-  position.x *= width
-  position.y *= height
-  var reach = new Vector(width, height).multiply(this.reach)
 
   for (var i = 0; i < checks.length; i++)
-    if (!checks[i](position, reach, width, height))
+    if (!checks[i](this.position, this.reach))
       return false
   return true
 }
@@ -256,75 +306,86 @@ function Explosion(speed, surface){
 }
 
 // Player class --------------------------------------------------------
-function Player(){
-  this.speed = 0.001
-  this.position = new Vector(0.5, 0.5)
-  this.reach = 0.05
-  this.lives = 3
+function Player(screen){
+  this.speed = new Vector(0, 0)
+  this.maxForce = 0.01
+  this.force = 0.001
 
-  var self = this
+  this.reach = 0.05
+  this.direction = 0
+  this.position = new Vector(0.5, 0.5)
+  var reach = this.reach
   this.surface = [
     new Vector(0.35, 0), new Vector(-0.25, 0.25), new Vector(-0.25, -0.25)
-  ].map(function(v){ return v.multiply(self.reach) })
+  ].map(function(v){ return v.multiply(reach) })
+
+  this.render = getRender(this, screen)
 }
 
-Player.prototype.collision = function(object){
-  if (this.position.distance(object.position) > Math.max(this.reach, object.reach))
-    return false
-
-  var self = this
-  surface = this.surface.map(function(v){ return v.add(self.position) })
-  a = surface[0]; b = surface[1]; c = surface[2]
-  for (var i = 0; i < this.surface; i += 5) { // @todo test this
-    var p = this.surface[i]
-    var v0 = c.add(a.multiply(-1)),
-        v1 = b.add(a.multiply(-1)),
-        v2 = p.add(a.multiply(-1))
-
-    var dot00 = v0.multiply(v0),
-        dot01 = v0.multiply(v1),
-        dot02 = v0.multiply(v2),
-        dot11 = v1.multiply(v1),
-        dot12 = v1.multiply(v2)
-
-    var invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
-
-    var u = (dot11 * dot02 - dot01 * dot12) * invDenom,
-        v = (dot00 * dot12 - dot01 * dot02) * invDenom
-
-    if ((u >= 0) && (v >= 0) && (u + v < 1))
-      return true
-    // credits go to http://www.blackpawn.com/texts/pointinpoly/default.html
-  }
-  return false
-}
-
-Player.prototype.rotate = function(degrees) {
-  this.surface = this.surface.map(function(v) {
+Player.prototype.rotate = function(degrees, screen){
+  this.surface = this.surface.map(function(v){
     return new Vector(
       v.x * Math.cos(degrees) - v.y * Math.sin(degrees),
       v.x * Math.sin(degrees) + v.y * Math.cos(degrees)
     )
   })
+
+  this.render = getRender(this, screen)
+}
+
+// Projectile class ----------------------------------------------------
+function Projectile(position, direction, screen){
+  this.position = position
+  this.direction = direction
+  this.speed = 4
+
+  this.reach = 0.0015
+  this.surface = []
+  var angle
+  for (var i = 0; i < 8; i++){
+    angle = i / 8 * 2 * Math.PI
+    this.surface.push(
+      new Vector(Math.cos(angle), Math.sin(angle))
+        .multiply(this.reach)
+    )
+  }
+
+  this.render = getRender(this, screen)
+}
+
+Projectile.prototype.onScreen = function(){
+  return (
+    this.position.x > 0 && this.position.x < 1 &&
+    this.position.y > 0 && this.position.y < 1
+  )
 }
 
 // Utility functions ---------------------------------------------------
-function draw(object, ctx, screenWidth, screenHeight, pen) {
-  surface = object.surface.map(function(v){
-    var u = v.add(object.position)
-    u.x *= screenWidth
-    u.y *= screenHeight
-    return u
+function getRender(object, screen){
+  return {
+    position: new Vector(
+      object.position.x * screen.width,
+      object.position.y * screen.height
+    ),
+    surface: object.surface.map(function(v){
+      return v.multiply(screen.scale)
+    })
+  }
+}
+
+function draw(object, ctx, pen){
+  points = object.render.surface.map(function(v){
+    return v.add(object.render.position)
   })
 
   ctx.beginPath()
-  ctx.moveTo(surface[0].x, surface[0].y)
-  for (var i = 1; i < surface.length; i++) {
-    ctx.lineTo(surface[i].x, surface[i].y)
+  ctx.moveTo(points[0].x, points[0].y)
+  for (var i = 1; i < points.length; i++){
+    ctx.lineTo(points[i].x, points[i].y)
   }
   ctx.closePath()
 
-  if (pen.fill) {
+  if (pen.fill){
     ctx.fillStyle = pen.fill
     ctx.fill()
   }
@@ -335,7 +396,28 @@ function draw(object, ctx, screenWidth, screenHeight, pen) {
   }
 }
 
-function baseLog(x, y) {
+function move(object, screen){
+  if (typeof(object.speed) == 'number')
+    object.position = object.position.add(
+      new Vector(
+        Math.cos(object.direction),
+        -Math.sin(object.direction)
+      ).multiply(object.speed / screen.scale)
+    )
+  else
+    object.position = object.position.add(object.speed)
+
+  object.render.position = new Vector(
+    object.position.x * screen.width,
+    object.position.y * screen.height
+  )
+}
+
+function collides(object, other){
+  return object.position.distance(other.position) < Math.max(object.reach, other.reach)
+}
+
+function baseLog(x, y){
   return Math.log(y) / Math.log(x)
 }
 
@@ -345,15 +427,19 @@ function Vector(x, y){
   this.y = y
 }
 
-Vector.prototype.length = function(){
+Vector.prototype.magnitude = function(){
   return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+}
+
+Vector.prototype.normalized = function(){
+  return this.multiply(1 / this.magnitude())
 }
 
 Vector.prototype.distance = function(other){
   return other.add(this.multiply(-1)).length()
 }
 
-Vector.prototype.add = function(other) {
+Vector.prototype.add = function(other){
   if (other.constructor == Vector)
     return new Vector(this.x + other.x, this.y + other.y)
   else
